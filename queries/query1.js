97efc431b260b70da18f9d014700f2e0e180334e
William Riser <riser.w@northeastern.edu>
2024-04-07 21:35:16 -0700

    Added diagrams

--- queries/query1.js
+++ queries/query1.js
@@ -3,12 +3,13 @@ const client = await MongoClient.connect("mongodb://localhost:27017/");
 const db = client.db("project2");
 const collection = db.collection("players");
 
-// Finds the average age and height of players by position
+// Finds the average age, weight, and height of players by position
 const positions = [
   {
     $group: {
       _id: "$position",
       avgAge: { $avg: "$age" },
+      avgWeight: { $avg: "$weight" },
       avgHeight: { $avg: "$height" },
       count: { $sum: 1 },
     },
